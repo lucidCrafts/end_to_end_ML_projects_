0ade6333f6ba45e12af132f74f48981a135b8ae3
Varun <varunvijay969@gmail.com>
2023-10-06 20:22:24 -0400

    Resolved preprocessor issue impacting model accuracy

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import numpy as np 
 import pandas as pd
@@ -12,105 +11,105 @@ from src.logger import logging
 from src.utils import save_object
 from src.exception import CustomException
 from src.utils import load_object
+from src.config import PipelineConfig
+
+from pathlib import Path
 
 
+PREPROCESSOR_OBJ_FILE_PATH=PipelineConfig().preprocessor_path
 
-@dataclass
-class DataTransformationConfig:
-    
-    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor_obj.pkl")
+
+import numpy as np
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import RobustScaler
+from pathlib import Path
+import sys
+import logging
+
+# Assuming these are defined somewhere:
+from src.exception import CustomException
+from src.config import PipelineConfig
+from src.utils import save_object
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config =DataTransformationConfig()
+        self.data_transformation_config = PipelineConfig()
 
     def get_data_transformer_object(self):
-        '''
-        This function si responsible for data trnasformation
-        
-        '''
+        """
+        Returns a data transformer object
+        """
         try:
-            numerical_columns = ["Time","Amount"]
-          
-            num_pipeline= Pipeline(
+            # Scaling only "Time" and "Amount"
+            numerical_columns = ["Time", "Amount"]
+
+            num_pipeline = Pipeline(
                 steps=[
-                ("scaler",RobustScaler())
-
-                ])
-          
-
-            logging.info(f"Numerical columns: {numerical_columns}")
-
-            preprocessor_obj=ColumnTransformer(
-                [
-                ("num_pipeline",num_pipeline,numerical_columns),
-                                ]
+                    ("scaler", RobustScaler())
+                ]
             )
-            
-                              
+
+            # V1-V28 don't require any transformation, but need to be retained
+            feature_columns = [
+                "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10",
+                "V11", "V12", "V13", "V14", "V15", "V16", "V17", "V18", "V19", "V20",
+                "V21", "V22", "V23", "V24", "V25", "V26", "V27", "V28"
+            ]
+
+            # Building the Column Transformer
+            preprocessor_obj = ColumnTransformer(
+                transformers=[
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("pass_through", 'passthrough', feature_columns)
+                ],
+                remainder='drop'  # Droping other columns in the dataframe
+            )
+
             return preprocessor_obj
         
-        
-        
         except Exception as e:
-            raise CustomException(e,sys)
-        
+            raise CustomException(e, sys)
+
     def initiate_data_transformation(self, train_path, test_path, val_path):
-
+        """
+        Loads data, transforms it, and saves the transformation object
+        """
         try:
-            train_df=pd.read_csv(train_path)
-            test_df=pd.read_csv(test_path)
-            val_df =pd.read_csv(val_path)
-            
-            preprocessing_obj =self.get_data_transformer_object()
-               
-            logging.info("Read train and test data completed")
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            val_df = pd.read_csv(val_path)
 
-            logging.info("Obtaining preprocessing object")
+            preprocessing_obj = self.get_data_transformer_object()
 
-            columns_to_scale = ["Time", "Amount"]
+            # Separating input and target features
+            X_train = train_df.drop(columns="Class")
+            y_train = train_df["Class"]
 
-            input_feature_train_df=train_df.drop(columns="Class",axis=1)
-            target_feature_train_df=train_df["Class"]
+            X_test = test_df.drop(columns="Class")
+            y_test = test_df["Class"]
 
-            input_feature_test_df=test_df.drop(columns="Class",axis=1)
-            target_feature_test_df=test_df["Class"]
-            
-            input_feature_val_df = val_df.drop(columns="Class",axis=1)
-            target_feature_val_df = val_df["Class"]
+            X_val = val_df.drop(columns="Class")
+            y_val = val_df["Class"]
 
-            
+            # Transforming datasets
+            X_train_transformed = preprocessing_obj.fit_transform(X_train)
+            X_test_transformed = preprocessing_obj.transform(X_test)
+            X_val_transformed = preprocessing_obj.transform(X_val)
 
-            logging.info(
-                f"Applying preprocessing object on training dataframe and testing dataframe."
-            )
+            # Merginging transformed features and target
+            train_arr = np.c_[X_train_transformed, y_train.to_numpy()]
+            test_arr = np.c_[X_test_transformed, y_test.to_numpy()]
+            val_arr = np.c_[X_val_transformed, y_val.to_numpy()]
 
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
-            input_feature_val_arr=preprocessing_obj.transform(input_feature_val_df)
-            
-
-            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
-            val_arr = np.c_[input_feature_val_arr, np.array(target_feature_val_df)]
-            
-                
-                    
-            logging.info(f"Saved preprocessing object.")
-
-            preprocessing_obj = save_object(
-
-                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+            # Saving the preprocessing object for later use
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_path,
                 obj=preprocessing_obj
-
-
             )
 
-            return (
-                train_arr,
-                test_arr,
-                val_arr,
-                self.data_transformation_config.preprocessor_obj_file_path,
-            )
+            return train_arr, test_arr, val_arr
+
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
